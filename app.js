af154f6597803fac2579daae5e8545a69ccfe1b7
Dylane <tanolamene@gmail.com>
2024-12-21 15:28:33 -0400

    Mise en place des models et des controllers

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
-const express = require('express');
-const morgan = require('morgan');
-const path = require('path');
+const express = require("express");
+const morgan = require("morgan");
+const path = require("path");
 const index = require("./routes");
 require("./database");
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+exports.app = app;
+require("./config/session.config");
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
--- database/index.js
+++ database/index.js
@@ -1,20 +1,21 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-const {
-  MONGODB_USER,
-  MONGODB_PASSWORD,
-  MONGODB_CLUSTER,
-  MONGODB_DATABASE,
-} = process.env;
+const { MONGODB_USER, MONGODB_PASSWORD, MONGODB_CLUSTER, MONGODB_DATABASE } =
+  process.env;
 
-mongoose
+const clientPromise = mongoose
   .connect(
     `mongodb+srv://${MONGODB_USER}:${MONGODB_PASSWORD}@${MONGODB_CLUSTER}/${MONGODB_DATABASE}?retryWrites=true&w=majority`
   )
   .then(() => {
-    console.log(`✅ Connecté à ${MONGODB_DATABASE} avec succès! [${new Date().toLocaleString()}]`);
+    console.log(
+      `✅ Connecté à ${MONGODB_DATABASE} avec succès! [${new Date().toLocaleString()}]`
+    );
+    return mongoose.connection.getClient();
   })
   .catch((err) => {
     console.log("❌ Failed to connect to database", err.message);
   });
+
+exports.clientPromise = clientPromise;
